f262e44d4cfe0307a8605e500d1eacc2c06f82be
seowooda <koeun0409@gmail.com>
2024-07-12 00:44:53 +0900

    feat: TextArea 컴포넌트 chore: tailwind.config Pretendard 폰트 추가

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+'use client'
+import { Label } from '@/components/ui/label'
+import { Textarea } from '@/components/ui/textarea'
+import { useState } from 'react'
 
-const TextArea = () => {
-  return <div>TextArea</div>;
-};
+interface TextareaWithTextProps {
+  label: string
+  maxLength?: number
+}
 
-export default TextArea;
+export function TextareaWithText({
+  label,
+  maxLength = 100,
+}: TextareaWithTextProps) {
+  const [text, setText] = useState<string>('')
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(event.target.value)
+  }
+
+  return (
+    <div className="grid w-full gap-1.5 text-black font-Pretendard">
+      <Label htmlFor="message-2" className="text-base">
+        {label}
+      </Label>
+      <Textarea
+        id="message-2"
+        placeholder={label}
+        value={text}
+        onChange={handleChange}
+        className="placeholder:text-gray-400"
+      />
+      <p className="text-sm text-muted-foreground text-right font-normal leading-5">
+        <span className="text-black">{text.length}</span>
+        <span className="text-gray-400">/{maxLength}</span>
+      </p>
+    </div>
+  )
+}
