56cf1d6affd2630bee492bf8f3d75ce12952d261
seowooda <koeun0409@gmail.com>
2024-07-11 21:51:35 +0900

    feat: component/input

--- src/components/Input/InputForm.css.ts
+++ src/components/Input/InputForm.css.ts
@@ -1,1 +1,9 @@
-//test
+const InputFormStyles = {
+  container: 'flex flex-col items-start gap-1.5 self-stretch',
+  label: 'text-black font-pretendard text-base font-normal leading-7',
+  input:
+    'flex, items-center self-stretchrounded-md border border-gray-300 bg-white',
+  placeholder:
+    'placeholder:text-gray-400 placeholder:font-pretendard placeholder:text-sm placeholder:font-normal placeholder:leading-5',
+}
+export default InputFormStyles
