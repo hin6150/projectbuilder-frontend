d196cde293f7cf36dc6412acc0dd2f1a70b7a98e
seowooda <koeun0409@gmail.com>
2024-08-12 23:54:05 +0900

    feat: color-picker 추가

--- src/app/(afterLogin)/schedule/layout.tsx
+++ src/app/(afterLogin)/schedule/layout.tsx
@@ -4,14 +4,31 @@ import { CalendarHeader } from '@/components/Header/CalendarHeader'
 import { Calendar } from '@/components/ui/calendar'
 import { useCalendar } from '@/hooks/useCalendar'
 import { usePathname } from 'next/navigation'
-import { Monthly } from '@/components/Calendar/Monthly'
+import { useProjectInfoQuery } from '@/api'
+import { HexColorPicker } from 'react-colorful'
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from '@/components/ui/popover'
+import { Button } from '@/components/ui/button'
 
 type ViewType = 'monthly' | 'weekly' | 'list'
 
-const layout = ({ children }: { children: React.ReactNode }) => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   const { state, handlePrev, handleNext, handleToday } = useCalendar()
   const pathname = usePathname()
   const view = (pathname.split('/').pop() || 'list') as ViewType
+  const { data } = useProjectInfoQuery()
+
+  const [colors, setColors] = React.useState<{ [key: string]: string }>({})
+
+  const handleColorChange = (uid: string, newColor: string) => {
+    setColors((prevColors) => ({
+      ...prevColors,
+      [uid]: newColor,
+    }))
+  }
 
   const getHandlersForView = (view: ViewType) => {
     switch (view) {
@@ -35,11 +52,39 @@ const layout = ({ children }: { children: React.ReactNode }) => {
   }
 
   const { onPrev, onNext } = getHandlersForView(view)
-  console.log('Layout - state.date:', state.date)
 
   return (
-    <div className="m-auto flex h-screen w-[1180px]">
-      <Calendar />
+    <div className="m-auto flex w-[1180px]">
+      <div className="flex flex-col gap-6">
+        <Calendar className="h-[315px] rounded-[8px] border border-gray-300 text-large" />
+        <div className="flex flex-col gap-3 p-[10px]">
+          <p className="text-body">내 캘린더</p>
+          {data?.result.map((project) => (
+            <div className="flex items-center gap-2" key={project.uid}>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button
+                    style={{
+                      backgroundColor: colors[project.uid] || '#aabbcc',
+                    }}
+                    className="h-[14px] w-[14px] rounded-[2px] border border-gray-200 p-0"
+                  />
+                </PopoverTrigger>
+                <PopoverContent className="w-[235px]">
+                  <HexColorPicker
+                    color={colors[project.uid] || '#aabbcc'}
+                    onChange={(newColor) =>
+                      handleColorChange(project.uid, newColor)
+                    }
+                  />
+                </PopoverContent>
+              </Popover>
+              <p className="text-small">{project.title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
       <div className="p-4">
         {/* <CalendarHeader
           view={view}
@@ -48,11 +93,10 @@ const layout = ({ children }: { children: React.ReactNode }) => {
           onNext={onNext}
           onToday={handleToday}
         /> */}
-
         {children}
       </div>
     </div>
   )
 }
 
-export default layout
+export default Layout
