88ce2a095e392d3a415dda3684370130a83e13b7
hin6150 <hin6150@gmail.com>
2024-09-09 17:47:45 +0900

    feat: mixed content 오류 proxy 설정

--- src/api/lib/fetcher.ts
+++ src/api/lib/fetcher.ts
@@ -52,8 +52,8 @@ export class Fetcher {
         },
       }
 
-      const response = await fetch(`${this.baseUrl}${url}`, fetchOptions)
-      // const response = await fetch(url, fetchOptions)
+      // const response = await fetch(`${this.baseUrl}${url}`, fetchOptions)
+      const response = await fetch(url, fetchOptions)
 
       if (response.ok) {
         data = (await response.json()) as Promise<ResponseType>
@@ -99,8 +99,8 @@ export class Fetcher {
         },
       }
 
-      const response = await fetch(`${this.baseUrl}${url}`, fetchOptions)
-      // const response = await fetch(url, fetchOptions)
+      // const response = await fetch(`${this.baseUrl}${url}`, fetchOptions)
+      const response = await fetch(url, fetchOptions)
 
       console.log(`Bearer ${client.getQueryData<string>(['access'])!}`)
 
